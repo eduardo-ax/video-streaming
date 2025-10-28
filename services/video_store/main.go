ef448a5bc9e720795df66b4fe043201f94f0d1ca
eduardo-ax <eduardo.axps@gmail.com>
2025-10-28 09:06:44 -0300

    feature: inject database dependency into VideoManager object

--- services/video_store/main.go
+++ services/video_store/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/eduardo-ax/video-streaming/services/video_store/api"
 	"github.com/eduardo-ax/video-streaming/services/video_store/domain"
+	"github.com/eduardo-ax/video-streaming/services/video_store/infrastructure"
 	"github.com/labstack/echo"
 )
 
@@ -10,7 +11,14 @@ func main() {
 	echoServer := echo.New()
 	v1Group := echoServer.Group("/v1")
 
-	videoUpload := &domain.VideoManager{}
+	pool := infrastructure.NewPool()
+	db := infrastructure.NewDatabase(pool)
+	defer db.Close()
+
+	pub := infrastructure.NewPublisher()
+	defer pub.Close()
+
+	videoUpload := domain.NewVideoManager(db, pub)
 	handler := api.NewVideoHandler(videoUpload)
 	handler.Register(v1Group)
 
