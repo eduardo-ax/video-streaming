a7ad3722710df3c291d594788d70cab97a3e74e4
eduardo-ax <eduardo.axps@gmail.com>
2025-10-28 09:05:59 -0300

    feature: create storage interface and injection db dependency

--- services/video_store/domain/video_store.go
+++ services/video_store/domain/video_store.go
@@ -23,10 +23,34 @@ type VideoUploader interface {
 	Store(ctx context.Context, title string, description string, content []byte) error
 }
 
-type VideoManager struct{}
+type VideoManager struct {
+	db  Storage
+	pub MessagePublisher
+}
+
+func NewVideoManager(db Storage, pub MessagePublisher) *VideoManager {
+	return &VideoManager{
+		db: db,
+	}
+}
 
 func (v *VideoManager) Store(ctx context.Context, title string, description string, content []byte) error {
 
 	fmt.Printf("Saving video: %s, Description: %s, Size: %d bytes\n", title, description, len(content))
+	id, err := v.db.Persist(ctx, title, description)
+
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Video saved with ID: %d\n", id)
+	v.pub.SendMessage(fmt.Sprintf("teste", "teste"), content)
 	return nil
+}
+
+type Storage interface {
+	Persist(ctx context.Context, title string, description string) (int, error)
+}
+
+type MessagePublisher interface {
+	SendMessage(key string, value []byte) error
 }
