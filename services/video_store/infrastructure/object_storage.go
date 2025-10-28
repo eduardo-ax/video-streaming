6fdb4a252bcd2cea76dce0a49086e63bea14143f
Eduardo Alexandre <eduardoalexandree.ps>
2025-10-28 17:39:23 -0300

    feature: created object_storage.go file and s3 conection

---
+++ services/video_store/infrastructure/object_storage.go
@@ -0,0 +1,49 @@
+package infrastructure
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"mime/multipart"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type ObjectStore struct {
+	client *s3.Client
+	bucket string
+}
+
+func NewObjectStore(client *s3.Client, bucket string) *ObjectStore {
+	return &ObjectStore{
+		client: client,
+		bucket: bucket,
+	}
+}
+
+func (o *ObjectStore) UploadVideo(ctx context.Context, file *multipart.FileHeader) (string, error) {
+	src, err := file.Open()
+	if err != nil {
+		return "", err
+	}
+	defer src.Close()
+
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(src); err != nil {
+		return "", err
+	}
+
+	key := fmt.Sprintf("videos/%s", file.Filename)
+	_, err = o.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(o.bucket),
+		Key:         aws.String(key),
+		Body:        bytes.NewReader(buf.Bytes()),
+		ContentType: aws.String(file.Header.Get("Content-Type")),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return key, nil
+}
