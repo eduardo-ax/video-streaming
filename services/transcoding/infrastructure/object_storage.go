56576c80a35827f2d0a87065005967e4e37ae3cb
eduardo.axps <eduardo.axps@gmail.com>
2025-10-31 17:24:47 -0300

    feat: add func, to save video into docker path

--- services/transcoding/infrastructure/object_storage.go
+++ services/transcoding/infrastructure/object_storage.go
@@ -7,7 +7,7 @@ import (
 	"io"
 	"mime/multipart"
 	"os"
-
+	"path/filepath"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
@@ -25,7 +25,10 @@ func NewObjectStore(client *s3.Client, bucket string) *ObjectStore {
 }
 
 func (o *ObjectStore) DownloadFile(ctx context.Context, id int, filename string) (string, error) {
-	key := fmt.Sprintf("videos/%d/%s", id, filename)
+	fmt.Println("Downloading file from S3:")
+	fmt.Printf("videos/%s\n", filename)
+
+	key := fmt.Sprintf("videos/%s", filename)
 
 	out, err := o.client.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: aws.String(o.bucket),
@@ -36,21 +39,26 @@ func (o *ObjectStore) DownloadFile(ctx context.Context, id int, filename string)
 	}
 	defer out.Body.Close()
 
-	// salva local
-	localPath := fmt.Sprintf("./%s", filename)
+	basePath := "/var/videos"
+	localPath := filepath.Join(basePath, fmt.Sprintf("%d", id), filename)
+	if err := os.MkdirAll(filepath.Dir(localPath), 0755); err != nil {
+		return "", err
+	}
+
 	f, err := os.Create(localPath)
 	if err != nil {
 		return "", err
 	}
 	defer f.Close()
 
-	_, err = io.Copy(f, out.Body)
-	if err != nil {
+	if _, err = io.Copy(f, out.Body); err != nil {
 		return "", err
 	}
 
 	return localPath, nil
 }
+
+
 
 func (o *ObjectStore) UploadFile(ctx context.Context, file *multipart.FileHeader, id int) error {
 	src, err := file.Open()
