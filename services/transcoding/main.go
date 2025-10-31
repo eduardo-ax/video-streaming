6a36c9c9c6b32937f28a6b79cabf7d94642afed6
eduardo.axps <eduardo.axps@gmail.com>
2025-10-31 17:22:52 -0300

    feat: add aws s3 client

--- services/transcoding/main.go
+++ services/transcoding/main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/eduardo-ax/video-streaming/services/transcoding/domain"
 	"github.com/eduardo-ax/video-streaming/services/transcoding/infrastructure"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	pool := infrastructure.NewPool()
+	defer pool.Close()
+	db := infrastructure.NewDatabase(pool)
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	s3Client := s3.NewFromConfig(cfg)
+	bucketName := os.Getenv("S3_BUCKET_NAME")
+	objectStore := infrastructure.NewObjectStore(s3Client, bucketName)
+
 	producer, err := infrastructure.NewProducer()
 	if err != nil {
 		log.Fatal(err)
@@ -16,11 +31,11 @@ func main() {
 	defer producer.Close()
 
 	ctx := context.Background()
+	videoTranscoder := domain.NewVideoTranscoder(db, producer, objectStore)
 
 	err = producer.ReceiveMessage(ctx, func(msg string) {
-		// quando chega mensagem do Kafka, chama o domínio
-		if err := domain.TranscodeVideo(msg); err != nil {
-			log.Printf("Erro ao transcodificar: %v", err)
+		if err := videoTranscoder.TranscodeVideo(ctx, msg); err != nil {
+			log.Printf("transcoding error %v", err)
 		}
 	})
 	if err != nil {
