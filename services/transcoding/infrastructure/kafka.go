db979f63c151d5e1cdd20ef1eef64ef81ad5225a
eduardo-ax <eduardo.axps@gmail.com>
2025-10-31 00:22:17 -0300

    feat: create infrastructure file

---
+++ services/transcoding/infrastructure/kafka.go
@@ -0,0 +1,79 @@
+package infrastructure
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/IBM/sarama"
+)
+
+const (
+	KAFKA_BROKER_URL = "localhost:9092"
+	KAFKA_TOPIC      = "full_video"
+)
+
+type Publisher struct {
+	syncProducer sarama.SyncProducer
+}
+
+func (p *Publisher) Close() error {
+	return p.syncProducer.Close()
+}
+
+func NewProducer() (*Publisher, error) {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+	config.Version = sarama.V3_0_0_0
+	syncProducer, err := sarama.NewSyncProducer([]string{KAFKA_BROKER_URL}, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Sarama producer: %w", err)
+	}
+
+	return &Publisher{
+		syncProducer: syncProducer,
+	}, nil
+}
+
+func (p *Publisher) SendMessage(ctx context.Context, key string) error {
+	msg := &sarama.ProducerMessage{
+		Topic: KAFKA_TOPIC,
+		Key:   sarama.StringEncoder(key),
+	}
+	_, _, err := p.syncProducer.SendMessage(msg)
+	return err
+}
+
+func (p *Publisher) ReceiveMessage(ctx context.Context, handler func(msg string)) error {
+	config := sarama.NewConfig()
+	config.Version = sarama.V3_0_0_0
+	config.Consumer.Return.Errors = true
+
+	consumer, err := sarama.NewConsumer([]string{KAFKA_BROKER_URL}, config)
+	if err != nil {
+		return fmt.Errorf("failed to create consumer: %w", err)
+	}
+	defer consumer.Close()
+
+	partitionConsumer, err := consumer.ConsumePartition(KAFKA_TOPIC, 0, sarama.OffsetNewest)
+	if err != nil {
+		return fmt.Errorf("failed to consume partition: %w", err)
+	}
+	defer partitionConsumer.Close()
+
+	fmt.Println("Listening to topic:", KAFKA_TOPIC)
+
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			fmt.Printf("Message received: %s\n", string(msg.Value))
+			handler(string(msg.Value))
+		case err := <-partitionConsumer.Errors():
+			fmt.Println("Consumer error:", err)
+		case <-ctx.Done():
+			fmt.Println("Context canceled, stopping consumer.")
+			return nil
+		}
+	}
+}
---
+++ services/transcoding/infrastructure/object_storage.go
@@ -0,0 +1,78 @@
+package infrastructure
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type ObjectStore struct {
+	client *s3.Client
+	bucket string
+}
+
+func NewObjectStore(client *s3.Client, bucket string) *ObjectStore {
+	return &ObjectStore{
+		client: client,
+		bucket: bucket,
+	}
+}
+
+func (o *ObjectStore) DownloadFile(ctx context.Context, id int, filename string) (string, error) {
+	key := fmt.Sprintf("videos/%d/%s", id, filename)
+
+	out, err := o.client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(o.bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return "", err
+	}
+	defer out.Body.Close()
+
+	// salva local
+	localPath := fmt.Sprintf("./%s", filename)
+	f, err := os.Create(localPath)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, out.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return localPath, nil
+}
+
+func (o *ObjectStore) UploadFile(ctx context.Context, file *multipart.FileHeader, id int) error {
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	buf := new(bytes.Buffer)
+	if _, err := buf.ReadFrom(src); err != nil {
+		return err
+	}
+
+	key := fmt.Sprintf("videos/%s", fmt.Sprintf("%d-", id)+file.Filename)
+	_, err = o.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket:      aws.String(o.bucket),
+		Key:         aws.String(key),
+		Body:        bytes.NewReader(buf.Bytes()),
+		ContentType: aws.String(file.Header.Get("Content-Type")),
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
