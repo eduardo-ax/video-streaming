7d44d85bb16c8bb054fa0b64f59c8b822d627908
eduardo.axps <eduardo.axps@gmail.com>
2025-11-11 17:14:38 -0300

    feat: create revoke session, renew session, user logout handlers and domain functions

--- services/user/api/api.go
+++ services/user/api/api.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/eduardo-ax/video-streaming/services/user/domain"
 	"github.com/labstack/echo/v4"
@@ -20,8 +21,17 @@ type LoginUserRequest struct {
 	Password string `json:"password"`
 }
 
+type RenewAccessTokenReq struct {
+	RefreshToken string `json:"refresh_token"`
+}
+
 type UserHandler struct {
 	user domain.UserInterface
+}
+
+type RenewAccessTokenRes struct {
+	AccessToken         string    `json:"access_token"`
+	AcessTokenExpiresAt time.Time `json:"acess_token_expires_at"`
 }
 
 func NewUserHander(user domain.UserInterface) *UserHandler {
@@ -33,6 +43,9 @@ func NewUserHander(user domain.UserInterface) *UserHandler {
 func (u *UserHandler) Register(e *echo.Group) {
 	e.POST("/user", u.CreateUserHandler)
 	e.POST("/login", u.LoginHandler)
+	e.POST("/logout/:id", u.LogoutHandler)
+	e.POST("/renew", u.RenewTokenHandler)
+	e.POST("/revoke/:id", u.RevokeTokenHandler)
 }
 
 func (u *UserHandler) CreateUserHandler(c echo.Context) error {
@@ -49,7 +62,6 @@ func (u *UserHandler) CreateUserHandler(c echo.Context) error {
 	}
 
 	return echo.NewHTTPError(http.StatusCreated, "user created successfully")
-
 }
 
 func (u *UserHandler) LoginHandler(c echo.Context) error {
@@ -61,11 +73,61 @@ func (u *UserHandler) LoginHandler(c echo.Context) error {
 	}
 
 	userClaims, err := u.user.UserLogin(ctx, userLogin.Email, userLogin.Password)
-
 	if err != nil {
 		return echo.NewHTTPError(http.StatusUnauthorized, "incorrect credentials")
 	}
 
-	fmt.Println(userClaims)
-	return echo.NewHTTPError(http.StatusOK, "login successfully")
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "login successfully",
+		"user":    userClaims,
+	})
+}
+
+func (u *UserHandler) LogoutHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+	id := c.Param("id")
+
+	if id == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid request payload")
+	}
+
+	err := u.user.UserLogout(ctx, id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error logout user")
+	}
+	return echo.NewHTTPError(http.StatusOK, "logout successfully")
+}
+
+func (u *UserHandler) RenewTokenHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+	refreshToken := &RenewAccessTokenReq{}
+	if err := c.Bind(refreshToken); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid request payload")
+	}
+
+	req, err := u.user.RenewAccessToken(ctx, refreshToken.RefreshToken)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error renew  token")
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "renew successfully",
+		"user":    req,
+	})
+
+}
+
+func (u *UserHandler) RevokeTokenHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+	id := c.Param("id")
+
+	if id == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid request payload")
+	}
+
+	err := u.user.RevokeSession(ctx, id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Error logout user")
+	}
+	return echo.NewHTTPError(http.StatusOK, "session revoke successfuly")
 }
--- services/user/domain/user.go
+++ services/user/domain/user.go
@@ -8,39 +8,26 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-type User struct {
-	ID       string
-	Name     string
-	Email    string
-	Password string
-	Plan     string
+type Storage interface {
+	Persist(ctx context.Context, name string, email string, password string, plan int8) (string, error)
+	GetUser(ctx context.Context, email string) (*UserAuthData, error)
+	CreateSession(ctx context.Context, session *Session) (*Session, error)
+	GetSession(ctx context.Context, id string) (*Session, error)
+	DeleteSession(ctx context.Context, id string) error
+	RevokeSession(ctx context.Context, id string) error
 }
 
-type UserPayload struct {
-	ID    string
-	Email string
-	Plan  int8
-}
-
-type UserAuthData struct {
-	ID       string
-	Password string
-	Plan     int8
-}
-
-type LoginUserRes struct {
-	AccessToken string      `json:"access_token"`
-	User        UserPayload `json:"user"`
+type TokenInterface interface {
+	CreateToken(id string, email string, plan int8, duration time.Duration) (string, *UserClaims, error)
+	VerifyToken(tokenStr string) (*UserClaims, error)
 }
 
 type UserInterface interface {
 	CreateUser(ctx context.Context, name string, email string, plan int8, pass string) error
 	UserLogin(ctx context.Context, email string, password string) (*LoginUserRes, error)
-}
-
-type UserManager struct {
-	db    Storage
-	token TokenInterface
+	UserLogout(ctx context.Context, id string) error
+	RenewAccessToken(ctx context.Context, refreshToken string) (*RenewAccessTokenRes, error)
+	RevokeSession(ctx context.Context, id string) error
 }
 
 func NewUserManager(db Storage, token TokenInterface) *UserManager {
@@ -51,18 +38,15 @@ func NewUserManager(db Storage, token TokenInterface) *UserManager {
 }
 
 func (u *UserManager) CreateUser(ctx context.Context, name string, email string, plan int8, password string) error {
-
 	cryptPassword, err := HashPassword(password)
 	if err != nil {
 		return err
 	}
 	_, err = u.db.Persist(ctx, name, email, cryptPassword, plan)
-
 	if err != nil {
 		return err
 	}
 	return nil
-
 }
 
 func (u *UserManager) UserLogin(ctx context.Context, email string, password string) (*LoginUserRes, error) {
@@ -75,20 +59,87 @@ func (u *UserManager) UserLogin(ctx context.Context, email string, password stri
 		return nil, fmt.Errorf("password incorrect")
 	}
 
-	acessToken, err := u.token.CreateToken(user.ID, email, user.Plan, 15*time.Minute)
-
+	acessToken, accessClaims, err := u.token.CreateToken(user.ID, email, user.Plan, 15*time.Minute)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create token: %w", err)
 	}
+
+	refreshToken, refreshClaim, err := u.token.CreateToken(user.ID, email, user.Plan, 24*time.Hour)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create refresh token: %w", err)
+	}
+
+	session, err := u.db.CreateSession(ctx, &Session{
+		ID:           refreshClaim.RegisteredClaims.ID,
+		Email:        email,
+		RefreshToken: refreshToken,
+		IsRevoked:    false,
+		ExpiresAt:    refreshClaim.RegisteredClaims.ExpiresAt.Time,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error creating session: %w", err)
+	}
 	return &LoginUserRes{
-		AccessToken: acessToken,
+		SessionID:             session.ID,
+		AccessToken:           acessToken,
+		RefreshToken:          refreshToken,
+		AccessTokenExpiresAt:  accessClaims.RegisteredClaims.ExpiresAt.Time,
+		RefreshTokenExpiresAt: refreshClaim.RegisteredClaims.ExpiresAt.Time,
 		User: UserPayload{
 			ID:    user.ID,
 			Email: email,
 			Plan:  user.Plan,
 		},
 	}, nil
+}
 
+func (u *UserManager) UserLogout(ctx context.Context, id string) error {
+	err := u.db.DeleteSession(ctx, id)
+	if err != nil {
+		return fmt.Errorf("logout error %w", err)
+	}
+	return nil
+}
+
+func (u *UserManager) RenewAccessToken(ctx context.Context, refreshToken string) (*RenewAccessTokenRes, error) {
+
+	refreshClaims, err := u.token.VerifyToken(refreshToken)
+	if err != nil {
+		return nil, fmt.Errorf("error verifying token: %w", err)
+	}
+
+	session, err := u.db.GetSession(ctx, refreshClaims.RegisteredClaims.ID)
+	if err != nil {
+		return nil, fmt.Errorf("error getting session: %w", err)
+	}
+
+	if session.IsRevoked {
+		return nil, fmt.Errorf("session revoked: %w", err)
+	}
+
+	if session.Email != refreshClaims.Email {
+		return nil, fmt.Errorf("invalid session")
+	}
+
+	acessToken, accessClaims, err := u.token.CreateToken(refreshClaims.ID, refreshClaims.Email, refreshClaims.Plan, 15*time.Minute)
+
+	if err != nil {
+		return nil, fmt.Errorf("error creating token: %w", err)
+	}
+
+	return &RenewAccessTokenRes{
+		AccessToken:         acessToken,
+		AcessTokenExpiresAt: accessClaims.RegisteredClaims.ExpiresAt.Time,
+	}, nil
+}
+
+func (u *UserManager) RevokeSession(ctx context.Context, id string) error {
+	err := u.db.RevokeSession(ctx, id)
+	if err != nil {
+		return fmt.Errorf("error revoking session %w", err)
+	}
+	return nil
 }
 
 func HashPassword(pass string) (string, error) {
@@ -99,14 +150,4 @@ func HashPassword(pass string) (string, error) {
 func CheckPassword(pass, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(pass))
 	return err == nil
-}
-
-type Storage interface {
-	Persist(ctx context.Context, name string, email string, password string, plan int8) (string, error)
-	GetUser(ctx context.Context, email string) (*UserAuthData, error)
-}
-
-type TokenInterface interface {
-	CreateToken(id string, email string, plan int8, duration time.Duration) (string, error)
-	VerifyToken(tokenStr string) (*UserPayload, error)
 }
