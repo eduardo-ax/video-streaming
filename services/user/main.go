874ac00ff5257d90d83b82e87e27e0d6a6c8647e
eduardo-ax <eduardo.axps@gmail.com>
2025-11-10 22:49:39 -0300

    feature. Add get secret key from .env and inject token dependency in domain object

--- services/user/main.go
+++ services/user/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/eduardo-ax/video-streaming/services/user/api"
 	"github.com/eduardo-ax/video-streaming/services/user/domain"
 	"github.com/eduardo-ax/video-streaming/services/user/infrastructure"
+	"github.com/eduardo-ax/video-streaming/services/user/token"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
+const minSecretKeySize = 32
+
 func main() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Println("Warning: Could not load .env file, falling back to environment variables.")
+	}
+
+	secretKey := os.Getenv("SECRET_KEY")
+
+	if len(secretKey) < minSecretKeySize {
+		log.Println("Warning: secret key size incorrect")
+	}
 
 	pool := infrastructure.NewPool()
 	db := infrastructure.NewDatabase(pool)
 	defer db.Close()
 
-	u := domain.NewUserManager(db)
+	token := token.NewJWTMaker(secretKey)
+
+	u := domain.NewUserManager(db, token)
 
 	handler := api.NewUserHander(u)
 
